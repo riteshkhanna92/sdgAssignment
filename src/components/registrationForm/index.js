94f0eb5645b4eb0e62343211445a3d2a49e0c238
Ritesh Khanna <Ritesh.Khanna@telusinternational.com>
2019-09-19 17:29:38 +0530

    commit after adding date picker

--- src/components/registrationForm/index.js
+++ src/components/registrationForm/index.js
@@ -1,7 +1,13 @@
-import React from 'react'
 import { Field, reduxForm } from 'redux-form'
+import React,{Component} from 'react'
+import DateTimePicker from 'react-widgets/lib/DateTimePicker'
+import moment from 'moment'
+import momentLocalizer from "react-widgets-moment"
 
-const required = value => value ? undefined : 'Required'
+import 'react-widgets/dist/css/react-widgets.css'
+
+momentLocalizer(moment)
+const required = (value, allValues, props, name) => value ? undefined : `${name} is Required`
 const maxLength = max => value =>
   value && value.length > max ? `Must be ${max} characters or less` : undefined
 const maxLength15 = maxLength(15)
@@ -17,9 +23,15 @@ const tooOld = value =>
 const aol = value =>
   value && /.+@aol\.com/.test(value) ?
   'Really? You still use AOL for your email?' : undefined
-
+  const renderDateTimePicker = ({ input: { onChange, value }, showTime }) =>
+  <DateTimePicker
+    onChange={onChange}
+    format="DD MMM YYYY"
+    time={showTime}
+    value={!value ? null : new Date(value)}
+  />
 const renderField = ({ input, label, type, meta: { touched, error, warning } }) => {
-    debugger
+     
     return (  <div>
         <label>{label}</label>
         <div>
@@ -30,30 +42,70 @@ const renderField = ({ input, label, type, meta: { touched, error, warning } }) 
 
 }
 
-const FieldLevelValidationForm = (props) => {
-  const { handleSubmit, pristine, reset, submitting } = props
+
+export class FieldLevelValidationForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          selectedOption: ["Finance"],
+    
+        };
+    
+         
+      }
+      handleDropDownSelect=(ev)=>{
+ 
+      }
+    
+     renderSelect = ({ input, label, type, meta: { touched, error, warning } }) => {
+          
+     
+        return (  <div>
+            <label>{label}</label>
+            <div>
+              <select  placeholder={label} value={this.state.selectedOption} multiple={true}  onChange={(e) => this.handleDropDownSelect(e)}>
+              <option value="Finance" dangerouslySetInnerHTML={{__html:'<input type="checkbox" name="vehicle1" />'}}></option>
+              {/* <option> <input type="checkbox" name="vehicle1" checked value="Finance"/></option>
+              <option><input type="checkbox" name="vehicle1" checked value="It"/></option>
+              <option><input type="checkbox" name="vehicle1" checked value="Operatons"/></option> */}
+              </select>
+              {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+            </div>
+          </div>)
+    
+    }
+  render()
+ 
+  {
+    const { handleSubmit, pristine, reset, submitting } = this.props
   return (
     <form onSubmit={handleSubmit}>
       <Field name="First Name" type="text"
-        component={renderField} label="Username"
+        component={renderField} label="First Name"
         validate={[ required, maxLength15 ]}
+        warn={aol}
       />
       <Field name="Contact" type="number"
         component={renderField} label="Contact"
         validate={email}
         warn={aol}
       />
-      <Field name="Date of Registration" type="date"
-        component={renderField} label="Age"
-        validate={[ required, number, minValue18 ]}
-        warn={tooOld}
-      />
+       {/* <Field
+          name="dob"
+          showTime={false}
+          component={renderDateTimePicker}
+        />
+         <Field name="Select Departments"  
+        component={this.renderSelect} label="Select Departments"
+        
+      /> */}
       <div>
         <button type="submit" disabled={submitting}>Submit</button>
         <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
       </div>
     </form>
   )
+}
 }
 
 export default reduxForm({
